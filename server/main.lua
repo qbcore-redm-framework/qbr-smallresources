32a17cae92b51c43e4e9a7167c7526e4140b61c6
vanillaa <80897991+evanillaa@users.noreply.github.com>
2022-02-02 12:02:59 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -3,5 +3,9 @@ QBCore.Commands.Add("id", "Check Your ID #", {}, false, function(source, args)
 end)
 
 QBCore.Functions.CreateCallback('smallresources:server:GetCurrentPlayers', function(source, cb)
-    cb(#QBCore.Functions.GetPlayers())
+    local TotalPlayers = 0
+    for k, v in pairs(QBCore.Functions.GetPlayers()) do
+        TotalPlayers = TotalPlayers + 1
+    end
+    cb(TotalPlayers)
 end)
