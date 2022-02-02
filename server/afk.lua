8c3bb54b70d8f3b3726f4f6487ece307bdb87384
vanillaa <80897991+evanillaa@users.noreply.github.com>
2022-02-02 12:01:46 +0200

    Update afk.lua

--- server/afk.lua
+++ server/afk.lua
@@ -1,6 +1,6 @@
 local QBCore = exports['qbr-core']:GetCoreObject()
 
-RegisterNetEvent('KickForAFK', function()
+RegisterNetEvent('qbr-afkkick:server:KickForAFK', function()
     local src = source
 	DropPlayer(src, 'You Have Been Kicked For Being AFK')
 end)
