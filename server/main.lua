640abe24daa529dd08ee0606c5668e1ea023ccb4
RexShack <rexshackuk@gmail.com>
2022-06-22 16:21:30 +0100

    Update main.lua
    get citizen ID command

--- server/main.lua
+++ server/main.lua
@@ -2,6 +2,13 @@ exports['qbr-core']:AddCommand("id", "Check Your ID #", {}, false, function(sour
 	TriggerClientEvent('QBCore:Notify', source, 9,  "ID: "..source, 5000, 0, 'blips', 'blip_radius_search', 'COLOR_WHITE')
 end)
 
+exports['qbr-core']:AddCommand("cid", "Check Your Citizen ID #", {}, false, function(source)
+    local src = source
+	local Player = exports['qbr-core']:GetPlayer(src)
+	local Playercid = Player.PlayerData.citizenid
+	TriggerClientEvent('QBCore:Notify', source, 9,  "Citizen ID: "..Playercid, 5000, 0, 'blips', 'blip_radius_search', 'COLOR_WHITE')
+end)
+
 exports['qbr-core']:CreateCallback('smallresources:server:GetCurrentPlayers', function(source, cb)
     local TotalPlayers = 0
     for k, v in pairs(exports['qbr-core']:GetQBPlayers()) do
