409756ebd091be7faf82b47bddb950264dc32a71
Ryan <xENG710@gmail.com>
2022-06-23 11:40:40 -0400

    Fixed Eagle Eye Functionality
    This will fix the Eagle Eye mechanic.

--- client/eagleeye.lua
+++ client/eagleeye.lua
@@ -1,13 +1,14 @@
-local function EnableEagleeye(player, enable)
+function EnableEagleeye(player, enable)
 	Citizen.InvokeNative(0xA63FCAD3A6FEC6D2, player, enable)
 end
 
 AddEventHandler("playerSpawned", function(spawnInfo)
-	EnableEagleeye(PlayerPedId(), true)
+	EnableEagleeye(PlayerId(), true)
 end)
 
 AddEventHandler("onResourceStop", function(resourceName)
-    if (GetCurrentResourceName() == resourceName) then
-        EnableEagleeye(PlayerPedId(), false)
+	if GetCurrentResourceName() ~= resourceName then
+		return
 	end
+	EnableEagleeye(PlayerId(), false)
 end)
