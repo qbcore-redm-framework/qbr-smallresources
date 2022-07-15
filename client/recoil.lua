151f7779e3f86c25595c1464de68bf6976c93127
Vanadis <61510130+vanadis1@users.noreply.github.com>
2022-07-15 20:47:42 +0300

    Add fireworks and more optimize handsup
    - Add fireworks items and function. (Not test multiple player)
    - Handshup move to ignore because one loop.
    - This pr included #20

--- client/recoil.lua
+++ client/recoil.lua
@@ -46,36 +46,40 @@ local recoils = {
 }
 
 
+
 Citizen.CreateThread(function()
 	while true do
 		local ped = PlayerPedId()
-		if IsPedShooting(ped) then --and not IsPedDoingDriveby(ped) then
-			local _,wep = GetCurrentPedWeapon(ped)
-			if recoils[wep] and recoils[wep] ~= 0 then
-				tv = 0
-				-- if GetFollowPedCamViewMode() ~= 4 then
-				-- 	repeat
-				-- 		Wait(0)
-				-- 		p = GetGameplayCamRelativePitch()
-				-- 		SetGameplayCamRelativePitch(p+0.1, 0.2)
-				-- 		tv = tv+0.1
-				-- 	until tv >= recoils[wep]
-				--else
-					repeat
-						Wait(0)
-						p = GetGameplayCamRelativePitch()
-						if recoils[wep] > 0.1 then
-							SetGameplayCamRelativePitch(p+0.6, 1.2)
-							tv = tv+0.6
-						else
-							SetGameplayCamRelativePitch(p+0.016, 0.333)
-							tv = tv+0.1
-						end
-					until tv >= recoils[wep]
-				--end
+		if Citizen.InvokeNative(0x8425C5F057012DAB,ped) ~= GetHashKey("WEAPON_UNARMED") then
+			if IsPedShooting(ped) then --and not IsPedDoingDriveby(ped) then
+				local _,wep = GetCurrentPedWeapon(ped)
+				if recoils[wep] and recoils[wep] ~= 0 then
+					tv = 0
+					-- if GetFollowPedCamViewMode() ~= 4 then
+					-- 	repeat
+					-- 		Wait(0)
+					-- 		p = GetGameplayCamRelativePitch()
+					-- 		SetGameplayCamRelativePitch(p+0.1, 0.2)
+					-- 		tv = tv+0.1
+					-- 	until tv >= recoils[wep]
+					--else
+						repeat
+							Wait(0)
+							p = GetGameplayCamRelativePitch()
+							if recoils[wep] > 0.1 then
+								SetGameplayCamRelativePitch(p+0.6, 1.2)
+								tv = tv+0.6
+							else
+								SetGameplayCamRelativePitch(p+0.016, 0.333)
+								tv = tv+0.1
+							end
+						until tv >= recoils[wep]
+					--end
+				end
 			end
+			Citizen.Wait(1)
+		else
+			Citizen.Wait(2000)
 		end
-
-		Citizen.Wait(0)
 	end
 end)
