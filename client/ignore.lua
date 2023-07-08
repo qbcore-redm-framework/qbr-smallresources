3883c1004d61f8a914a2380f6bf8bd89175e60c2
DosKit <56743692+DosKit@users.noreply.github.com>
2023-07-08 14:59:45 +0100

    Create Game Event Triggers
    This will give the option to add game events to trigger a created event. This is to help isolate the need for alot of resources to loop to get different events.

--- client/ignore.lua
+++ client/ignore.lua
@@ -1,14 +1,31 @@
--- removes events and challenge notfications
+-- removes events or create Event Triggers to Utilize instead needing to loop in each resource
+local GameEvents = {
+    [`EVENT_LOOT_COMPLETE`] = 'Looted', ---- Example This will trigger event QBCore:Event:Looted and include data for ped and the entity interacting with
+}
+
+local function DataViewEvent(eventGroup, index, argStructSize)
+    local buffer = string.rep("\0", 16)
+    Citizen.InvokeNative(0x57EC5FA4D4D6AFCA, eventGroup, index, buffer, argStructSize)
+    local data = {}
+    data.ped = string.unpack("i4", buffer, 0)
+    data.target = string.unpack("i4", buffer, 9)
+    return data
+end
+
 CreateThread(function()
     local DisableEvents = Config.DisableEvents
     while true do
         Wait(10)
-        local size = GetNumberOfEvents(0)   
+        local size = GetNumberOfEvents(0)
         if size > 0 then
             for i = 0, size - 1 do
                 local eventAtIndex = GetEventAtIndex(0, i)
                 if DisableEvents[eventAtIndex] then 
                     Citizen.InvokeNative(0x6035E8FBCA32AC5E)
+                elseif GameEvents[eventAtIndex] then
+                    local current = GameEvents[eventAtIndex]
+                    local data = DataViewEvent(0, i, 3)
+                    TriggerEvent('QBCore:Event:'..current, data)
                 end
             end
         end
